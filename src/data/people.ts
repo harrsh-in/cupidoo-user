60c5135edbfb5f2eea144026c8046e6df373dce5
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-24 18:20:04 +0530

    Added dashboard

---
+++ src/data/people.ts
@@ -0,0 +1,60 @@
+const PEOPLE = [
+    {
+        id: '0',
+        name: 'Harrsh Patel',
+        age: 24,
+        gender: 'male',
+        profilePicture:
+            'https://images.pexels.com/photos/3754285/pexels-photo-3754285.jpeg',
+    },
+    {
+        id: '1',
+        name: 'Riya Patel',
+        age: 24,
+        gender: 'female',
+        profilePicture:
+            'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg',
+    },
+    {
+        id: '4',
+        name: 'John Carl',
+        age: 20,
+        gender: 'male',
+        profilePicture:
+            'https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg',
+    },
+    {
+        id: '2',
+        name: 'Jack Doe',
+        age: 26,
+        gender: 'male',
+        profilePicture:
+            'https://images.pexels.com/photos/943235/pexels-photo-943235.jpeg',
+    },
+    {
+        id: '3',
+        name: 'Rebecca Givens',
+        age: 26,
+        gender: 'female',
+        profilePicture:
+            'https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg',
+    },
+    {
+        id: '5',
+        name: 'Sofia Ava',
+        age: 20,
+        gender: 'female',
+        profilePicture:
+            'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg',
+    },
+    {
+        id: '6',
+        name: 'Robert',
+        age: 28,
+        gender: 'male',
+        profilePicture:
+            'https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg',
+    },
+];
+
+export default PEOPLE;
--- src/private/dashboard/index.tsx
+++ src/private/dashboard/index.tsx
@@ -1,9 +1,54 @@
-import { Box, Typography } from '@mui/material';
+import { Paper, Box, Typography } from '@mui/material';
+import { useState } from 'react';
+import PEOPLE from '../../data/people';
 
 const Dashboard = () => {
+    const [people] = useState(PEOPLE);
+
     return (
-        <Box>
-            <Typography>Dashboard</Typography>
+        <Box
+            sx={{
+                padding: '3rem 0',
+            }}
+        >
+            <Box className="people-list">
+                {people.map((person) => {
+                    return (
+                        <Paper
+                            elevation={0}
+                            variant="outlined"
+                            key={person.id}
+                            sx={{
+                                display: 'flex',
+                                gap: '1rem',
+                                padding: '1rem 2rem',
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    width: '10rem',
+                                    height: '10rem',
+                                }}
+                            >
+                                <Box
+                                    component="img"
+                                    src={person.profilePicture}
+                                    alt={person.name}
+                                    sx={{
+                                        width: '100%',
+                                        height: '100%',
+                                        objectFit: 'contain',
+                                    }}
+                                />
+                            </Box>
+                            <Box>
+                                <Typography>{person.name}</Typography>
+                                <Typography>{person.age}</Typography>
+                            </Box>
+                        </Paper>
+                    );
+                })}
+            </Box>
         </Box>
     );
 };
