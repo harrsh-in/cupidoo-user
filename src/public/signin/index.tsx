777ab8829b6a053cf8ba31a892b5c5b4c89b0b37
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-24 16:02:42 +0530

    Added form

--- src/public/signin/index.tsx
+++ src/public/signin/index.tsx
@@ -1,9 +1,131 @@
-import { Box, Typography } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import {
+    Box,
+    Button,
+    FormControl,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    TextField,
+    Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+
+interface IFormInput {
+    email: string;
+    password: string;
+}
 
 const SignIn = () => {
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    });
+
+    const onSubmit: SubmitHandler<IFormInput> = (data) => {
+        console.log(data);
+    };
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (
+        event: React.MouseEvent<HTMLButtonElement>
+    ) => {
+        event.preventDefault();
+    };
+
     return (
-        <Box>
-            <Typography>Sign In</Typography>
+        <Box
+            sx={{
+                maxWidth: '25rem',
+                marginX: 'auto',
+                paddingTop: '5rem',
+            }}
+        >
+            <Typography
+                variant="h4"
+                gutterBottom
+                sx={{
+                    textAlign: 'center',
+                }}
+            >
+                Sign In
+            </Typography>
+
+            <Box
+                component="form"
+                noValidate
+                autoComplete="off"
+                onSubmit={handleSubmit(onSubmit)}
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '1.2rem',
+                    marginTop: '3rem',
+                }}
+            >
+                <Controller
+                    name="email"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            autoComplete="email"
+                            label="Email"
+                            variant="outlined"
+                            required
+                        />
+                    )}
+                />
+
+                <Controller
+                    name="password"
+                    control={control}
+                    render={({ field }) => (
+                        <FormControl variant="outlined" required>
+                            <InputLabel htmlFor="outlined-adornment-password">
+                                Password
+                            </InputLabel>
+                            <OutlinedInput
+                                {...field}
+                                autoComplete="current-password"
+                                id="outlined-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={
+                                                handleMouseDownPassword
+                                            }
+                                            edge="end"
+                                        >
+                                            {showPassword ? (
+                                                <VisibilityOff />
+                                            ) : (
+                                                <Visibility />
+                                            )}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                        </FormControl>
+                    )}
+                />
+
+                <Button type="submit" variant="contained">
+                    Sign In
+                </Button>
+            </Box>
         </Box>
     );
 };
