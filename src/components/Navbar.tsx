da405c946f59744c5b1b8bc48357914b6f8613e3
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-29 18:03:51 +0530

    Updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import { Box, Typography } from '@mui/material';
 import { Fragment } from 'react';
-import Logo from '../assets/logo.png';
-import { removeUserToken } from '../utils/handleUserToken';
 import { useNavigate } from 'react-router-dom';
-import { useAppDispatch } from '../redux/hooks';
+import Logo from '../assets/logo.png';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { resetUserDetails } from '../redux/slice/user.slice';
+import { removeUserToken } from '../utils/handleUserToken';
 
 const Navbar = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
+
+    const { details } = useAppSelector((state) => state.user);
 
     const handleSignOut = () => {
         removeUserToken();
@@ -21,13 +23,46 @@ const Navbar = () => {
             <Box className="navbar-container" component="nav">
                 <img src={Logo} alt="Cupidoo" className="logo" />
 
-                <Box>
-                    <Typography
-                        className="navbar-links"
-                        onClick={handleSignOut}
-                    >
-                        Sign Out
-                    </Typography>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '1rem',
+                    }}
+                >
+                    {details ? (
+                        <Fragment>
+                            <Typography
+                                className="navbar-links"
+                                onClick={handleSignOut}
+                            >
+                                Sign Out
+                            </Typography>
+
+                            <Typography
+                                className="navbar-links"
+                                onClick={() => navigate('/profile')}
+                            >
+                                Profile
+                            </Typography>
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <Typography
+                                className="navbar-links"
+                                onClick={() => navigate('/signin')}
+                            >
+                                Sign In
+                            </Typography>
+
+                            <Typography
+                                className="navbar-links"
+                                onClick={() => navigate('/signup')}
+                            >
+                                Sign Up
+                            </Typography>
+                        </Fragment>
+                    )}
                 </Box>
             </Box>
 
