f97484ce805173f8e3caeff8629d2766740f7a36
Harrsh Patel <dev.harrsh@gmail.com>
2023-05-31 13:50:49 +0530

    Added stepper

--- src/private/profile/BasicDetails/OtherDetails.tsx
+++ src/private/profile/BasicDetails/OtherDetails.tsx
@@ -3,12 +3,13 @@ import { Box, Button, InputAdornment, TextField } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs, { Dayjs } from 'dayjs';
 import _ from 'lodash';
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import schema, { SignInFormSchemaType } from './schema';
 import { useAppSelector } from '../../../redux/hooks';
+import ProfilePhoto from './ProfilePhoto';
+import schema, { SignInFormSchemaType } from './schema';
 
-const OtherDetails = () => {
+const OtherDetails = ({ handleNext }: IProps) => {
     const { details } = useAppSelector((state) => state.user);
 
     const maximumDate = dayjs().subtract(18, 'years');
@@ -48,6 +49,7 @@ const OtherDetails = () => {
             return;
         }
         console.log(data);
+        handleNext();
     };
 
     useEffect(() => {
@@ -61,168 +63,178 @@ const OtherDetails = () => {
     }, [details]);
 
     return (
-        <Box
-            noValidate
-            component="form"
-            autoComplete="off"
-            onSubmit={handleSubmit(onSubmit)}
-            sx={{
-                maxWidth: '330px',
-                marginX: 'auto',
-            }}
-        >
-            <Controller
-                name="username"
-                control={control}
-                render={({ field }) => (
-                    <TextField
-                        {...field}
-                        required
-                        fullWidth
-                        disabled
-                        label="Username"
-                        variant="outlined"
-                        error={Boolean(_.get(errors, 'username', ''))}
-                        helperText={_.get(errors, 'username.message', '')}
-                        margin="dense"
-                        InputProps={{
-                            endAdornment: (
-                                <Button variant="text">Update</Button>
-                            ),
-                        }}
-                    />
-                )}
-            />
+        <Box>
+            <ProfilePhoto />
 
-            <Controller
-                name="name"
-                control={control}
-                render={({ field }) => (
-                    <TextField
-                        {...field}
-                        required
-                        fullWidth
-                        autoComplete="name"
-                        label="Name"
-                        variant="outlined"
-                        error={Boolean(_.get(errors, 'name', ''))}
-                        helperText={_.get(errors, 'name.message', '')}
-                        margin="dense"
-                    />
-                )}
-            />
-
-            <Controller
-                name="email"
-                control={control}
-                render={({ field }) => (
-                    <TextField
-                        {...field}
-                        required
-                        fullWidth
-                        disabled
-                        margin="dense"
-                        autoComplete="email"
-                        label="Email"
-                        variant="outlined"
-                        InputProps={{
-                            endAdornment: (
-                                <Button variant="text">Update</Button>
-                            ),
-                        }}
-                        error={Boolean(_.get(errors, 'email', ''))}
-                        helperText={_.get(errors, 'email.message', '')}
-                    />
-                )}
-            />
-
-            <Controller
-                name="contactNumber"
-                control={control}
-                render={({ field }) => (
-                    <TextField
-                        {...field}
-                        required
-                        fullWidth
-                        disabled
-                        margin="dense"
-                        autoComplete="contactNumber"
-                        label="Contact Number"
-                        variant="outlined"
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment
-                                    position="start"
-                                    disableTypography
-                                    disablePointerEvents
-                                >
-                                    {_.get(details, 'countryPrefix', '+91')}
-                                </InputAdornment>
-                            ),
-                            endAdornment: (
-                                <Button variant="text">Update</Button>
-                            ),
-                        }}
-                        error={Boolean(_.get(errors, 'email', ''))}
-                        helperText={_.get(errors, 'email.message', '')}
-                    />
-                )}
-            />
-
-            <DatePicker
-                closeOnSelect
-                displayWeekNumber
-                showDaysOutsideCurrentMonth
-                format="DD/MM/YYYY"
-                label="Date of birth"
-                value={dob.value}
-                defaultValue={dob.value}
-                maxDate={maximumDate}
-                onChange={(newValue) => {
-                    setDob((prev) => {
-                        return {
-                            ...prev,
-                            error: '',
-                            value:
-                                dayjs(newValue).diff(maximumDate, 'days') > 0
-                                    ? maximumDate
-                                    : newValue,
-                        };
-                    });
-                    setValue(
-                        'dob',
-                        dayjs(
-                            dayjs(newValue).diff(maximumDate, 'days') > 0
-                                ? maximumDate
-                                : newValue
-                        ).format('DD-MM-YYYY')
-                    );
-                }}
-                slotProps={{
-                    textField: {
-                        helperText: dob.error,
-                        error: Boolean(dob.error),
-                    },
-                }}
+            <Box
+                noValidate
+                component="form"
+                autoComplete="off"
+                onSubmit={handleSubmit(onSubmit)}
                 sx={{
-                    marginTop: '8px',
-                    marginBottom: '4px',
-                    width: '100%',
-                }}
-            />
-
-            <Button
-                type="submit"
-                variant="outlined"
-                fullWidth
-                sx={{
-                    marginTop: '1.5rem',
+                    maxWidth: '330px',
+                    marginX: 'auto',
+                    marginTop: '3rem',
                 }}
             >
-                Save
-            </Button>
+                <Controller
+                    name="username"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            required
+                            fullWidth
+                            disabled
+                            label="Username"
+                            variant="outlined"
+                            error={Boolean(_.get(errors, 'username', ''))}
+                            helperText={_.get(errors, 'username.message', '')}
+                            margin="dense"
+                            InputProps={{
+                                endAdornment: (
+                                    <Button variant="text">Update</Button>
+                                ),
+                            }}
+                        />
+                    )}
+                />
+
+                <Controller
+                    name="name"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            required
+                            fullWidth
+                            autoComplete="name"
+                            label="Name"
+                            variant="outlined"
+                            error={Boolean(_.get(errors, 'name', ''))}
+                            helperText={_.get(errors, 'name.message', '')}
+                            margin="dense"
+                        />
+                    )}
+                />
+
+                <Controller
+                    name="email"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            required
+                            fullWidth
+                            disabled
+                            margin="dense"
+                            autoComplete="email"
+                            label="Email"
+                            variant="outlined"
+                            InputProps={{
+                                endAdornment: (
+                                    <Button variant="text">Update</Button>
+                                ),
+                            }}
+                            error={Boolean(_.get(errors, 'email', ''))}
+                            helperText={_.get(errors, 'email.message', '')}
+                        />
+                    )}
+                />
+
+                <Controller
+                    name="contactNumber"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            required
+                            fullWidth
+                            disabled
+                            margin="dense"
+                            autoComplete="contactNumber"
+                            label="Contact Number"
+                            variant="outlined"
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment
+                                        position="start"
+                                        disableTypography
+                                        disablePointerEvents
+                                    >
+                                        {_.get(details, 'countryPrefix', '+91')}
+                                    </InputAdornment>
+                                ),
+                                endAdornment: (
+                                    <Button variant="text">Update</Button>
+                                ),
+                            }}
+                            error={Boolean(_.get(errors, 'email', ''))}
+                            helperText={_.get(errors, 'email.message', '')}
+                        />
+                    )}
+                />
+
+                <DatePicker
+                    closeOnSelect
+                    displayWeekNumber
+                    showDaysOutsideCurrentMonth
+                    format="DD/MM/YYYY"
+                    label="Date of birth"
+                    value={dob.value}
+                    defaultValue={dob.value}
+                    maxDate={maximumDate}
+                    onChange={(newValue) => {
+                        setDob((prev) => {
+                            return {
+                                ...prev,
+                                error: '',
+                                value:
+                                    dayjs(newValue).diff(maximumDate, 'days') >
+                                    0
+                                        ? maximumDate
+                                        : newValue,
+                            };
+                        });
+                        setValue(
+                            'dob',
+                            dayjs(
+                                dayjs(newValue).diff(maximumDate, 'days') > 0
+                                    ? maximumDate
+                                    : newValue
+                            ).format('DD-MM-YYYY')
+                        );
+                    }}
+                    slotProps={{
+                        textField: {
+                            helperText: dob.error,
+                            error: Boolean(dob.error),
+                        },
+                    }}
+                    sx={{
+                        marginTop: '8px',
+                        marginBottom: '4px',
+                        width: '100%',
+                    }}
+                />
+
+                <Button
+                    type="submit"
+                    variant="outlined"
+                    fullWidth
+                    sx={{
+                        marginTop: '1.5rem',
+                    }}
+                >
+                    Save and Next
+                </Button>
+            </Box>
         </Box>
     );
 };
 
 export default OtherDetails;
+
+interface IProps {
+    handleNext: () => void;
+}
--- src/private/profile/index.tsx
+++ src/private/profile/index.tsx
@@ -1,11 +1,49 @@
-import { Box } from '@mui/material';
-import BasicDetails from './BasicDetails';
+import { Paper, Step, StepLabel, Stepper } from '@mui/material';
+import { ReactNode, useState } from 'react';
+import OtherDetails from './BasicDetails/OtherDetails';
+
+const steps = ['', '', '', ''];
 
 const Profile = () => {
+    const [activeStep, setActiveStep] = useState(0);
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
     return (
-        <Box>
-            <BasicDetails />
-        </Box>
+        <Paper
+            elevation={1}
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                marginX: 'auto',
+                gap: '2rem',
+                padding: '3rem 1rem',
+            }}
+            className="basic-details-container"
+        >
+            <Stepper activeStep={activeStep} alternativeLabel>
+                {steps.map((label, index) => {
+                    const stepProps: { completed?: boolean } = {};
+                    const labelProps: {
+                        optional?: ReactNode;
+                    } = {};
+
+                    return (
+                        <Step key={index} {...stepProps}>
+                            <StepLabel {...labelProps}>{label}</StepLabel>
+                        </Step>
+                    );
+                })}
+            </Stepper>
+
+            {activeStep === 0 ? <OtherDetails handleNext={handleNext} /> : null}
+        </Paper>
     );
 };
 
